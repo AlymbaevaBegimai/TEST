28934d7df0f6b980c877f15f326821404923b6b6
Begimai <sally.vatanabe.2002@gmail.com>
2021-05-09 08:06:54 +0600

    added the first task

---
+++ 1.py
@@ -0,0 +1,24 @@
+# Наследование позволяет создавать новый класс на основе уже существующего класса.
+
+class Car:
+    maker = ""
+    
+
+    def __init__(self, model, maker):
+        self.model = model
+        self.maker = maker
+    
+    
+
+class Honda(Car):
+    weight = 1000
+
+    def add(self, kg):
+        self.weight += kg
+
+car_1 = Car(model="Kalina", maker="Lada") 
+print(car_1.maker)
+
+car_2 = Honda(model="1234", maker='Kris')
+car_2.add(100)
+print(car_2.weight)
